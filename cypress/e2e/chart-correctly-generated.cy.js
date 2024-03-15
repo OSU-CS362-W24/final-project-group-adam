7bd39a7be083055c9e71aabd6ae340aacf01776c
Devin <macombdevin@gmail.com>
2024-03-15 15:17:56 -0700

    added save chart e2e tests

--- cypress/e2e/chart-correctly-generated.cy.js
+++ cypress/e2e/chart-correctly-generated.cy.js
@@ -1,2 +1,16 @@
 it('Chart is correctly generated', () => {
 })
+
+it('correctly saves a chart', () =>{
+    //use command to generate a chart
+    cy.chartGen();
+
+    //save the chart
+    cy.findByRole('button', {name: 'Save chart'}).click();
+
+    //nav to gallery
+    cy.findByText('Gallery').click();
+
+    //assert that the created chart should be there
+    cy.findByRole('img').should('exist');
+})
